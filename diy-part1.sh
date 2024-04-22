9c360c1e17a73c37cb1e247fb1da0b8986a1cf93
huanghitoy <huanghitoy@gmail.com>
2024-04-22 09:58:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,4 +6,14 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+rm -f ./feeds.conf.default
+echo 'src-git packages https://github.com/Lienol/openwrt-packages.git;22.03' >> ./feeds.conf.default
+echo 'src-git luci https://github.com/Lienol/openwrt-luci.git;22.03' >> ./feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git;openwrt-22.03' >> ./feeds.conf.default
+echo 'src-git telephony https://github.com/huanghitoy/telephony.git;master' >> ./feeds.conf.default
+echo 'src-git lienol https://github.com/Lienol/openwrt-package.git;main' >> ./feeds.conf.default
+echo 'src-git other https://github.com/Lienol/openwrt-package.git;other' >> ./feeds.conf.default
+echo 'src-git 3ginfo https://github.com/4IceG/luci-app-3ginfo-lite.git' >> ./feeds.conf.default
+echo 'src-git passwall_packages https://github.com/huanghitoy/openwrt-passwall-packages.git;main' >> ./feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> ./feeds.conf.default
